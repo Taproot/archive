28a7f05bc4e9b54c32ca7d37e9f94bf5f929d7ce
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-02-02 11:28:34 +0000

    Installed phpunit, got first test passing

--- tests/ArchiveTest.php
+++ tests/ArchiveTest.php
@@ -5,5 +5,9 @@ namespace Taproot;
 use PHPUnit_Framework_TestCase;
 
 class ArchiveTest extends PHPUnit_Framework_TestCase {
-	
+	public function testUrlToFilesystemPath() {
+		$this->assertEquals(urlToFilesystemPath('http://example.com'), 'http/example.com/');
+		$this->assertEquals(urlToFilesystemPath('https://example.com/things/more?stuff=blah#fragment'), 'https/example.com/things/more');
+		$this->assertEquals(urlToFilesystemPath('https://localhost:8000/some-local-file/'), 'https/localhost:8000/some-local-file');
+	}
 }
