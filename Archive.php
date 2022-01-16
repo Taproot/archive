9c6cf76fbe664ee2d5cb6303a18f3040880431b2
Barnaby Walters <barnaby@waterpigs.co.uk>
2022-01-16 15:58:16 +0100

    Upgraded to guzzlehttp 7

--- Archive.php
+++ Archive.php
@@ -3,9 +3,10 @@
 namespace Taproot;
 
 use DateTime;
-use Guzzle;
+use GuzzleHttp;
 use Icap\HtmlDiff\HtmlDiff;
 use Mf2;
+use Psr;
 
 function urlToFilesystemPath($url) {
 	$u = parse_url($url);
@@ -21,35 +22,19 @@ function urlToFilesystemPath($url) {
 		. (isset($u['path']) ? trim($u['path'], '/') : ''), '/');
 }
 
-// This shim switching based on hostname really belongs in mf2/shim.
-function mfForResponse(Guzzle\Http\Message\Response $resp) {
-	$html = $resp->getBody(true);
-	return Mf2\parse($html, $resp->getEffectiveUrl());
-}
-
-function hasMicroformats($resp) {
-	$mf = mfForResponse($resp);
-	return count($mf['items']) > 0;
-}
-
-function areCachablyDifferent(Guzzle\Http\Message\Response $oldResp, Guzzle\Http\Message\Response $newResp) {
-	if (hasMicroformats($oldResp) and hasMicroformats($newResp)) {
-		// if mf changed, probably significantly different enough to warrant a new cache entry
-		return mfForResponse($oldResp) != mfForResponse($newResp);
-	} else {
-		// look for last-updated header
-		if ($newResp->getLastModified() != null) {
-			$lastModified = new DateTime($newResp->getLastModified());
-			$lastArchived = new DateTime($oldResp->getDate());
-			
-			return $lastModified > $lastArchived;
-		}
+function areCachablyDifferent(Psr\Http\Message\ResponseInterface $oldResp, Psr\Http\Message\ResponseInterface $newResp) {
+	// look for last-updated header
+	if ($newResp->getHeaderLine('Last-modified') != null) {
+		$lastModified = new DateTime($newResp->getHeaderLine('Last-modified'));
+		$lastArchived = new DateTime($oldResp->getHeaderLine('Date'));
 		
-		$differ = new HtmlDiff($oldResp->getBody(true), $newResp->getBody(true), true);
-		$mods = $differ->outputDiff()->getModifications();
-		// to do : figure out a sensible threshold here
-		return max($mods) > 2;
+		return $lastModified > $lastArchived;
 	}
+	
+	$differ = new HtmlDiff((string) $oldResp->getBody(), (string) $newResp->getBody(), true);
+	$mods = $differ->outputDiff()->getModifications();
+	// to do : figure out a sensible threshold here
+	return max($mods) > 2;
 }
 
 class Archive {
@@ -72,15 +57,21 @@ class Archive {
 	
 	// returns [$response, exception]
 	public function archive($url, $forceUpdate = false) {
-		$client = new Guzzle\Http\Client();
+		$client = new GuzzleHttp\Client();
 		$archivePath = $this->basepathForUrl($url);
 		
+		$respEffectiveUri = null;
+
 		try {
-			$response = $client->get($url)->send();
-			if (!$response->isContentType('text/html') and !$response->isContentType('application/xhtml+xml')) {
+			$response = $client->get($url, [
+				'on_stats' => function (GuzzleHttp\TransferStats $stats) use (&$respEffectiveUri) {
+					$respEffectiveUri = (string) $stats->getEffectiveUri();
+				}
+			]);
+			if (!$response->getHeaderLine('Content-type') == 'text/html' and !$response->getHeaderLine('Content-type') == 'application/xhtml+xml') {
 				return [$response, null];
 			}
-		} catch (Guzzle\Common\Exception\GuzzleException $e) {
+		} catch (GuzzleHttp\Exception\ClientException $e) {
 			// to do: what is more useful, returning the error or the latest response? maybe both?
 			return [null, $e];
 		}
@@ -93,7 +84,7 @@ class Archive {
 			}
 		}
 		
-		$response->setHeader('X-Archive-Effective-URL', $response->getEffectiveUrl());
+		$response = $response->withHeader('X-Archive-Effective-URL', $respEffectiveUri);
 		
 		// archive
 		@mkdir($archivePath, 0777, true);
@@ -101,8 +92,14 @@ class Archive {
 		// to do: ensure the response has Content-location header
 		// to do: ensure the response has Date header
 		$p = $archivePath . DIRECTORY_SEPARATOR . $fetched->format('Y-m-d\THis');
-		file_put_contents($p . '.html', $response->getBody());
-		file_put_contents($p . '-headers.txt', $response->getRawHeaders());
+		file_put_contents($p . '.html', (string) $response->getBody());
+
+		$headers = '';
+		foreach ($response->getHeaders() as $name => $values) {
+			$headers .= $name . ': ' . implode(', ', $values) . "\n";
+		}
+
+		file_put_contents($p . '-headers.txt', $headers);
 		return [$response, null];
 	}
 	
@@ -136,9 +133,7 @@ class Archive {
 		$headers = file_get_contents($this->basepathForVersion($url, $version) . '-headers.txt');
 		$body = file_get_contents($this->basepathForVersion($url, $version) . '.html');
 		
-		$resp = Guzzle\Http\Message\Response::fromMessage($headers . $body);
-		$resp->setEffectiveUrl((string) $resp->getHeader('x-archive-effective-url') ?: $url);
-		$resp->removeHeader('x-archive-effective-url');
+		$resp = GuzzleHttp\Psr7\Message::parseResponse($headers . $body);
 		return $resp;
 	}
 	
